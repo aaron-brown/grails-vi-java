c2355115f226b905993c6d0c469ae0ebb6d5e30a
Aaron Brown <brown.aaron.lloyd@gmail.com>
2014-05-28 07:44:19 -0500

    Adding VI Java SDK 5.1 .

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,9 +21,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.18'
+        compile 'com.vmware:vijava:5.1'
     }
 
     plugins {
